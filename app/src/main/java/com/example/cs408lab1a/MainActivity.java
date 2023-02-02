c1c4aca7a046e170dd1d14f2f12ebac448963bb1
Aogletree1 <ogletree.austin@yahoo.com>
2023-02-01 23:15:14 -0600

    Second Commit.

--- app/src/main/java/com/example/cs408lab1a/MainActivity.java
+++ app/src/main/java/com/example/cs408lab1a/MainActivity.java
@@ -13,18 +13,19 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private ActivityMainBinding binding;
-    private Model model;
 
     private int NUM_WEAPONS;
     private int playerChoice;
-    private int computerChoice;
+    int computerChoice;
+    private int num;
+    private int  pWin = 0;
+    private int cWin = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        model = new Model();
         Log.d(TAG, "This is a DEBUG log message.");
         updateCounter();
 
@@ -34,34 +35,112 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         Log.i(TAG, "Button clicked!");
         String tag = view.getTag().toString();
-        int weaponNum = (int)(Math.random() * NUM_WEAPONS);
 
+        NUM_WEAPONS = Weapon.values().length;
+
+        int weaponNum = (int) (Math.random() * NUM_WEAPONS);
         Weapon w = Weapon.values()[weaponNum];
-        if (weaponNum == 1) {
-            computerChoice = 1;
+        computerChoice = w.ordinal();
+        binding.score.setText(String.valueOf("Player: " + pWin + ", Computer: " + cWin));
 
-        }
 
 
 
         if (tag.equals("rockB")) {
             playerChoice = 1;
             binding.pWeapon.setText(String.valueOf("Player's Weapon: Rock"));
+            for (int i = 0; i < playerChoice; ++i) {
+                weaponNum = (int) (Math.random() * NUM_WEAPONS);
+                w = Weapon.values()[weaponNum];
+                computerChoice = w.ordinal();
+            }
         }
         else if (tag.equals("paperB")) {
             playerChoice = 2;
             binding.pWeapon.setText(String.valueOf("Player's Weapon: Paper"));
+            for (int i = 0; i < playerChoice; ++i) {
+                weaponNum = (int) (Math.random() * NUM_WEAPONS);
+                w = Weapon.values()[weaponNum];
+                computerChoice = w.ordinal();
+            }
         }
         else if (tag.equals("scissorsB")) {
             playerChoice = 3;
             binding.pWeapon.setText(String.valueOf("Player's Weapon: Scissors"));
+            for (int i = 0; i < playerChoice; ++i) {
+                weaponNum = (int) (Math.random() * NUM_WEAPONS);
+                w = Weapon.values()[weaponNum];
+                computerChoice = w.ordinal();
+
+        }}
+            if (computerChoice == 0) {
+                binding.cWeapon.setText(String.valueOf("Computer's Weapon: " + w));
+
+
+            }
+
+            else if (computerChoice == 1) {
+                binding.cWeapon.setText(String.valueOf("Computer's Weapon: " + w));
+
+            }
+
+            else if (computerChoice == 2) {
+                binding.cWeapon.setText(String.valueOf("Computer's Weapon: " + w));
+
+            }
+
+        if (computerChoice == 0 && playerChoice == 1) {
+                binding.winOutput.setText(String.valueOf("Draw!"));
+
+            }
+
+        else if (computerChoice == 1 && playerChoice == 2) {
+            binding.winOutput.setText(String.valueOf("Draw!"));
+
         }
+
+        else if (computerChoice == 2 && playerChoice == 3) {
+            binding.winOutput.setText(String.valueOf("Draw!"));
+
+        }
+
+        else if (computerChoice == 0 && playerChoice == 2) {
+            binding.winOutput.setText(String.valueOf("Player Wins!"));
+                pWin++;
+        }
+
+        else if (computerChoice == 0 && playerChoice == 3) {
+                binding.winOutput.setText(String.valueOf("Computer Wins!"));
+                cWin++;
+            }
+
+        else if (computerChoice == 1 && playerChoice == 1) {
+                binding.winOutput.setText(String.valueOf("Computer Wins!"));
+            cWin++;
+            }
+
+        else if (computerChoice == 1 && playerChoice == 3) {
+            binding.winOutput.setText(String.valueOf("Player Wins!"));
+            pWin++;
+        }
+
+        else if (computerChoice == 2 && playerChoice == 1) {
+            binding.winOutput.setText(String.valueOf("Player Wins!"));
+            pWin++;
+        }
+
+        else if (computerChoice == 2 && playerChoice == 2) {
+            binding.winOutput.setText(String.valueOf("Computer Wins!"));
+            cWin++;
+        }
+
+
 
         updateCounter();
     }
 
     private void updateCounter() {
-        binding.score.setText(String.valueOf(model.getCounter()));
+        binding.score.setText(String.valueOf(("Player: " + pWin + ", Computer: " + cWin)));
     }
 
 }
